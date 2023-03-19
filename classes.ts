58bc985dc7ab93a585baf7416f6e7314216b150c
Jinwoo Park <zvzv9808@gmail.com>
2023-03-19 18:09:22 +0900

    4-1) Classes Recap

--- classes.ts
+++ classes.ts
@@ -2,17 +2,54 @@ abstract class User{
     constructor(
         private firstName: string,
         private lastName: string,
-        public nickName: string
+        protected nickName: string
     ) {}
+    abstract getNickName(): void
+
     getFullName(){
         return `${this.firstName} ${this.lastName}`;
     }
 }
 
 class Player extends User{
-
+    getNickName(): void {
+        console.log(this.nickName);
+    }
 }
 
 const playerNico = new Player("nico", "las", "니꼬");
 
 playerNico.getFullName();
+
+type Words = {
+    [key:string]: string
+}
+
+class Dict{
+    private words : Words
+    constructor(){
+        this.words = {}
+    }
+    add(word: Word){
+        if(this.words[word.term] === undefined){
+            this.words[word.term] = word.def;
+        }
+    }
+    def(term:string){
+        return this.words[term]
+    }
+}
+
+class Word{
+    constructor(
+        public term: string,
+        public def: string
+    ){}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+
+const dict = new Dict();
+
+dict.add(kimchi);
+dict.def("kimchi")
